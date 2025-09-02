e544f8c1da9079d38a7f6b221eb443d2aa8af793
senno adi <senno.adi2509@gmail.com>
2025-09-03 00:34:36 +0700

    change config

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,13 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  // base: "/",
+  base: "/portfolio/",
   plugins: [vue(), tailwindcss()],
   build: {
-    outDir: "dist", // folder hasil build (standar Vite)
-    assetsDir: "assets", // tempat asset statis
-    sourcemap: false, // optional: matikan sourcemap untuk production
+    outDir: ".", // folder hasil build (standar Vite)
+    // assetsDir: "assets", // tempat asset statis
+    // sourcemap: false, // optional: matikan sourcemap untuk production
   },
   server: {
     open: true, // auto open browser saat dev server
